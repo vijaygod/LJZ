6a77f775255e760d2edfde5bae2aec937cb72a44
vijaygod <svijay@163.com>
2016-04-17 13:18:16 +0800

    网站结构成型
    希望项目进行顺利

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+    //多模块访问设置
+    'MODULE_ALLOW_LIST' => array('Home','Admin'),
+    'DEFAULT_MODULE' => 'Home',
+);
---
+++ Application/Runtime/Cache/Admin/b595b808c2ebeeeb0478ffc30bb4e87a.php
@@ -0,0 +1,10 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>骆家庄|后台</title>
+</head>
+<body>
+
+</body>
+</html>
